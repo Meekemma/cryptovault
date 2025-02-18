0c267d8c5aa08710865e4be48b78b2543182e802
unknown <meekycute007@yahoo.com>
2025-02-18 15:52:43 +0100

    resolved dependency issue

--- minning/settings.py
+++ minning/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 
-ALLOWED_HOSTS = ['crypto-yyxl.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cryptovault-fh88.onrender.com', 'localhost', '127.0.0.1']
 
 
 
