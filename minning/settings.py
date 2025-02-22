7ec27513839dda1c97021130bb8fe2e29b171ad0
unknown <meekycute007@yahoo.com>
2025-02-22 13:30:31 +0100

    worked on the qrcode of the payment

--- minning/settings.py
+++ minning/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_coinpayments',
-
     'base.apps.BaseConfig',
     'management.apps.ManagementConfig',
     'payment.apps.PaymentConfig',
